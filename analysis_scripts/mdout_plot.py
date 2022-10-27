3673079bb9758c12a885a1c1897979fb9e210d1d
marcushock <m1hock@ucsd.edu>
2022-10-27 15:10:26 -0700

    Changed the file to be much more efficient by counting files first.

--- analysis_scripts/mdout_plot.py
+++ analysis_scripts/mdout_plot.py
@@ -2,81 +2,103 @@
 
 ### Created by Marcus Hock
 # Used for a quick plot of all of the default outputs from the MDOUT files from the CHARMM-GUI settings for AMBER
-# The script will look for .mdout files in the current directory where the script is ran from 
-
+# The script will look for .mdout files in the current directory where the script is ran from
 
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 from time import time
+import re
 
 
 
-i = 1
-column_names = ['NSTEP','TIME','TEMP','PRES','Etot','EKtot','EPtot','BOND','ANGLE','DIHED','UB','UB','IMP','CMAP','ONE_FOUR_NB','ONE_FOUR_EEL','VDWAALS','EELEC','EHBOND','RESTRAINT','EKCMT','VIRIAL','VOLUME','SURFTEN','Density']
-df = pd.DataFrame(columns = column_names)
-    
+def count_files():
+    i = 1
+    while True:
+        try:
+            f = open('step7_{}.mdout'.format(i))
+            i+=1
+            f.close()
+        except:
+            return i-1
 
-while True:
-    
-    try:
-        print('Opening file...')
-        tstart = time()
-        f = open('step7_{}.mdout'.format(i))
-        print('Opened file step7_{}.mdout'.format(i))
-    except:
-        print('Breaking at i = {}'.format(i))
-        break
-    
 
-    for line in f.readlines():
-        write_flag = False
-        if 'NSTEP' in line:
-            NSTEP = (line.split()[2])
-            TIME = (line.split()[5])
-            TEMP = (line.split()[8])
-            PRES = (line.split()[11])        
-        elif 'Etot' in line: 
-            Etot = (line.split()[2])
-            EKtot = (line.split()[5])
-            EPtot = (line.split()[8])
-        elif ' BOND' in line: 
-            BOND = (line.split()[2])
-            ANGLE = (line.split()[5])
-            DIHED = (line.split()[8])
-        elif ' UB' in line: 
-            UB = (line.split()[2])
-            IMP = (line.split()[5])
-            CMAP = (line.split()[8])
-        elif '1-4 NB' in line: 
-            ONE_FOUR_NB = (line.split()[3])
-            ONE_FOUR_EEL = (line.split()[7])
-            VDWAALS = (line.split()[10])
-        elif 'EELEC' in line: 
-            EELEC = (line.split()[2])
-            EHBOND = (line.split()[5])
-            RESTRAINT = (line.split()[8])
-        elif 'EKCMT' in line: 
-            EKCMT = (line.split()[2])
-            VIRIAL = (line.split()[5])
-            VOLUME = (line.split()[8])
-        elif 'SURFTEN' in line: 
-            SURFTEN = (line.split()[2])
-        elif 'Density' in line:
-            Density = (line.split()[2])
-            write_flag = True
-            #print(Density)
-        if write_flag:
-            new_list = np.array((NSTEP,TIME,TEMP,PRES,Etot,EKtot,EPtot,BOND,ANGLE,DIHED,UB,UB,IMP,CMAP,ONE_FOUR_NB,ONE_FOUR_EEL,VDWAALS,EELEC,EHBOND,RESTRAINT,EKCMT,VIRIAL,VOLUME,SURFTEN,Density),dtype = float)
-            new_line = pd.DataFrame(data =np.array([new_list]), columns = column_names)
-            df = pd.concat((df,new_line))
-        if 'A V E R A G E S' in line:
+def step_info(file_name):
+    f = open(file_name)
+    lines = f.readlines()
+    for line in lines:
+        if 'ntpr' in line:
+            step_size = int(re.search('=(.*),', line).group(1))
+            print(step_size)
             break
-    f.close()
-    print('Closing file (open {} seconds)'.format(time() - tstart))
+        if 'nstlim' in line:
+            simulation_length = int(re.search('=(.*),', line).group(1))
 
-    i+=1
-    
+    n_steps = simulation_length // step_size
+    f.close()
+
+    return step_size, n_steps
+
+
+
+def read_all_files(n_files, n_steps, n_columns):
+    mdout_array = np.zeros((n_steps * n_files, n_columns))
+    row_counter = 0
+    for i in range(1,n_files+1):
+        f = open('step7_{}.mdout'.format(i))
+        for line in f.readlines():
+            if 'NSTEP' in line:
+                NSTEP = (line.split()[2])
+                TIME = (line.split()[5])
+                TEMP = (line.split()[8])
+                PRES = (line.split()[11])
+            elif 'Etot' in line:
+                Etot = (line.split()[2])
+                EKtot = (line.split()[5])
+                EPtot = (line.split()[8])
+            elif ' BOND' in line:
+                BOND = (line.split()[2])
+                ANGLE = (line.split()[5])
+                DIHED = (line.split()[8])
+            elif ' UB' in line:
+                UB = (line.split()[2])
+                IMP = (line.split()[5])
+                CMAP = (line.split()[8])
+            elif '1-4 NB' in line:
+                ONE_FOUR_NB = (line.split()[3])
+                ONE_FOUR_EEL = (line.split()[7])
+                VDWAALS = (line.split()[10])
+            elif 'EELEC' in line:
+                EELEC = (line.split()[2])
+                EHBOND = (line.split()[5])
+                RESTRAINT = (line.split()[8])
+            elif 'EKCMT' in line:
+                EKCMT = (line.split()[2])
+                VIRIAL = (line.split()[5])
+                VOLUME = (line.split()[8])
+            elif 'SURFTEN' in line:
+                SURFTEN = (line.split()[2])
+            elif 'Density' in line:
+                Density = (line.split()[2])
+                mdout_array[row_counter] = (NSTEP,TIME,TEMP,PRES,Etot,EKtot,EPtot,BOND,ANGLE,DIHED,UB,UB,IMP,CMAP,ONE_FOUR_NB,ONE_FOUR_EEL,VDWAALS,EELEC,EHBOND,RESTRAINT,EKCMT,VIRIAL,VOLUME,SURFTEN,Density)
+                row_counter += 1
+            if 'A V E R A G E S' in line:
+                break
+
+
+    return mdout_array
+
+n_files = count_files()
+step_size, n_steps = step_info('step7_1.mdout')
+print('There are {} files.'.format(n_files))
+
+column_names = ['NSTEP','TIME','TEMP','PRES','Etot','EKtot','EPtot','BOND','ANGLE','DIHED','UB','UB','IMP','CMAP','ONE_FOUR_NB','ONE_FOUR_EEL','VDWAALS','EELEC','EHBOND','RESTRAINT','EKCMT','VIRIAL','VOLUME','SURFTEN','Density']
+n_columns = len(column_names)
+
+mdout = read_all_files(n_files, n_steps, n_columns)
+
+df = pd.DataFrame(mdout, columns = column_names)
+
 df.reset_index(drop = True, inplace = True)
 
 
@@ -94,8 +116,3 @@ for i in range(5):
 plt.tight_layout()
 
 plt.show()
-# In[88]:
-
-
-
-
